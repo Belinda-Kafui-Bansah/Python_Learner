4822bfa25cceb02ac02095993ed1bccc53fd4c73
Belinda-Kafui-Bansah <bbansah.k@gmail.com>
2025-09-22 21:03:15 +0000

    Code for name and age

---
+++ pytre.py
@@ -0,0 +1,7 @@
+print ('hello!')
+print ('What is your name?')
+Name= input()
+print ('The length of your name is ' + str(len(Name)))
+print ('You are welcome ' + Name)
+Age = input('How old are you? ')
+print ('Next year by this time, you will be ' + str(int(Age) + 1))
